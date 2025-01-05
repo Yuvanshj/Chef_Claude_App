a37999e19f1df1ce6b735bfd1a103dabd750c008
yuvi <vujj892@gmail.com>
2025-01-05 17:34:28 +0530

    Update v0.0.2
    added loading animations

--- components/IngredientsList.jsx
+++ components/IngredientsList.jsx
@@ -9,8 +9,8 @@ export default function IngredientsList(props){
             <h2>Ingredients on hand:</h2>
             <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
             
-            {props.ingredients.length > 3 ? <div className="get-recipe-container">
-                <div>
+            {props.ingredients.length > 3 ? <div  className="get-recipe-container" style={{ display: props.check ? "none" : "flex" }}>
+                <div ref={props.ref}>
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
